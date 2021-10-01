5cb65cd4ce9a9e9efcd318732dcf13b3d5ccfbc1
Pranshu Jainn <re.pranshu@gmail.com>
2021-10-01 13:01:14 +0530

    V1

---
+++ contracts/temp.js
@@ -0,0 +1,6 @@
+string[] firstWords = ["Naruto", "Tanjiro", "Eren", "Senku", "Sasuke", "Nezuko","Mikasa"];
+string[] secondWords = ["India", "Japan", "USA", "Canada", "Russia", "France"];
+string[] thirdWords = ["Melody", "Life", "Music", "Sacrifice", "Meditate", "Silence"];
+
+string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: monospace; font-size: 29px; }</style><rect width='100%' height='100%' fill='#6b27b0' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+string[] colors = ["#c95a14","#6b27b0", "#08C2A8", "black", "#50ab00", "#102661", "#615510"];
